8b0f11cc529558f3fb842105c103fabc532296e9
ThaoNGUYEN1411 <phuongthaolyon@gmail.com>
2024-03-31 14:51:48 +0200

    add format-date, format-type

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent, useState} from 'react';
 import Pokemon from '../models/pokemon';
+import  formatDate  from '../helpers/format-date';
 import './pokemon-card.css';
+import formatType from '../helpers/format-type';
 //declarer type de props
 
 type Props = {
@@ -19,10 +21,6 @@ export const PokemonCard:FunctionComponent<Props> = ({pokemon, borderColor="#009
     setColor("#f5f5f5")
   }
 
-  const formatDate = (date: Date): string =>{
-    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-  }
-
   return (
     <div className='col s6 m4' key={pokemon.id} onMouseEnter={showBorder} onMouseLeave={hideBorder}>
       <div className='card horizontal' style={{borderColor:color}}>
@@ -32,6 +30,9 @@ export const PokemonCard:FunctionComponent<Props> = ({pokemon, borderColor="#009
             <div className='card-content'>
               <p>{pokemon.name}</p>
               <p><small>{formatDate(pokemon.created)}</small></p>
+              {pokemon.types.map((type)=>(
+                <p key={type} className={formatType(type)} style={{margin:'5px'}}>{type}</p>
+              ))}
             </div>
           </div>
         </div>
